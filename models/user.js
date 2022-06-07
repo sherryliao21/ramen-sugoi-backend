06a27296afd14503619f61f7f5e968b22713fab1
Sherry Liao <sherry.liao@pioneerm.com>
2022-06-07 17:05:22 +0800

    feat: add user schema

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize')
+const { ramenDB } = require('../databases/mariaDB')
+
+const User = ramenDB.define(
+  'user',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      unique: true
+    },
+    full_name: {
+      type: DataTypes.STRING
+    },
+    nick_name: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    profile_pic: {
+      type: DataTypes.STRING,
+      defaultValue: ''
+    },
+    description: {
+      type: DataTypes.TEXT
+    },
+    isBanned: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    role_id: {
+      type: DataTypes.INTEGER
+    },
+    favorite_id: {
+      type: DataTypes.INTEGER
+    }
+  },
+  { paranoid: true }
+)
+
+module.exports = User
